42789b56f14cf8df866889bcecc191b18b179f32
sevings <binque@ya.ru>
2023-02-11 16:31:11 +0100

    implement add storage

--- server/service/server.go
+++ server/service/server.go
@@ -2,19 +2,33 @@ package service
 
 import (
 	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	pb "karma/gen/server"
 )
 
 type Service struct {
 	pb.UnimplementedServerServer
-	addr []string
+	conns   []grpc.ClientConnInterface
+	clients map[string]pb.ServerClient
 }
 
 func NewService() *Service {
-	return &Service{}
+	return &Service{
+		conns:   make([]grpc.ClientConnInterface, 0, 5),
+		clients: make(map[string]pb.ServerClient),
+	}
 }
 
 func (s *Service) AddStorage(ctx context.Context, req *pb.AddRequest) (*pb.AddReply, error) {
-	s.addr = append(s.addr, req.GetAddress())
+	conn, err := grpc.Dial(req.GetAddress(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return &pb.AddReply{}, err
+	}
+	c := pb.NewServerClient(conn)
+
+	s.conns = append(s.conns, conn)
+	s.clients[req.GetAddress()] = c
+
 	return &pb.AddReply{}, nil
 }
--- storage/cmd/main.go
+++ storage/cmd/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"github.com/spf13/afero"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pbMain "karma/gen/server"
 	pb "karma/gen/storage"
 	"karma/storage/service"
 	"log"
 	"net"
 )
 
-var port = flag.Int("port", 37000, "The server port")
+var host = flag.String("host", "127.0.0.1", "The grpc host")
+var port = flag.Int("port", 37000, "The grpc port")
+var server = flag.String("server", "127.0.0.1:37700", "The server address")
+var capacity = flag.Uint64("capacity", 1024*1024, "The storage capacity")
 
 func main() {
 	flag.Parse()
@@ -24,7 +30,29 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterStorageServer(s, service.NewService(afero.NewMemMapFs()))
 	log.Printf("server listening at %v", lis.Addr())
+
+	go addStorage()
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func addStorage() {
+	conn, err := grpc.Dial(*server, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+	c := pbMain.NewServerClient(conn)
+
+	req := &pbMain.AddRequest{
+		Address:  fmt.Sprintf("%s:%d", *host, *port),
+		Capacity: *capacity,
+	}
+
+	_, err = c.AddStorage(context.Background(), req)
+	if err != nil {
+		log.Fatalf("could not add storage: %v", err)
 	}
 }
